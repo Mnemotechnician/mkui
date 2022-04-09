2aac42ca6c913bd28ac82abf2accee0c94db6429
MHeMoTexHuK <daytimer253@gmail.com>
2022-04-09 22:31:05 +0300

    more useful stuff

--- lib/src/main/kotlin/ElementUtil.kt
+++ lib/src/main/kotlin/ElementUtil.kt
@@ -5,12 +5,25 @@ import arc.scene.*
 import arc.scene.ui.*
 import arc.scene.ui.layout.*
 
+/** Text of the label */
 inline var Label.text: CharSequence?
 	get() = getText()
 	set(text: CharSequence?) { setText(text) }
+
+/** Text of the label inside of the text button */
 inline var TextButton.text: CharSequence?
 	get() = label?.getText()
 	set(text: CharSequence?) { label?.setText(text) }
+
+/** Text of the text field */
+inline var TextField.text: String?
+	get() = getText()
+	set(text: String?) { setText(text) }
+
+/** Hint of the text field. Displayed when there's no text in the said field. */
+inline var TextField.hint: String?
+	get() = getMessageText()
+	set(hint: String?) = setMessageText(hint)
 
 /** Casts the element to the specified class or returns null if it's not an instance of this class. */
 inline fun <reified T: Element> Element.asOrNull() = this as? T
