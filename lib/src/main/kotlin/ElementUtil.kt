27af669aa92d429030d18337405c784d29e1743c
MHeMoTexHuK <daytimer253@gmail.com>
2022-06-28 18:05:21 +0300

    Added element search functions

--- lib/src/main/kotlin/ElementUtil.kt
+++ lib/src/main/kotlin/ElementUtil.kt
@@ -118,3 +118,38 @@ fun Group.deepShrink(shrinkParents: Boolean = false, invalidateParents: Boolean 
 		invalidateHierarchy()
 	}
 }
+
+/**
+ * Finds an element by its type and name. Can return the current element.
+ * If [elementName] is null, only the type is accounted for.
+ */
+inline fun <reified T: Element> Element.findOrNull(elementName: String? = null): T? {
+	if (this is T && elementName == name) return this
+
+	if (this !is Group) return null
+
+	// unfortunately, i can't just make an inline function recursive... and local functions inside of inline ones are not supported yet
+	lateinit var rec: Group.() -> T?
+	rec = rec@ {
+		for (child in children) {
+			if (child is T && (elementName == null || child.name == elementName)) return@rec child
+
+			if (child is Group) child.rec()?.also { return@rec it }
+		}
+
+		return@rec null
+	}
+
+	return rec()
+}
+
+/**
+ * Finds an element by its name and type. Can return the current element.
+ * If [elementName] is null, only the type is accounted for.
+ * 
+ * @throws IllegalArgumentException if there's no such element.
+ */
+inline fun <reified T: Element> Element.find(elementName: String? = null): T {
+	return findOrNull(elementName)
+		?: throw IllegalArgumentException("Element with type ${T::class}${if (elementName == null) "" else " and name '$elementName'"} was not found")
+}
